3bc2cddf0e3ff6bcb92e268f98174bb590d76f59
Natalie Queally <nataliequeally@gmail.com>
2021-03-31 11:57:12 -0500

    added multiplication

--- math.py
+++ math.py
@@ -5,8 +5,16 @@ def sum(a,b):
     c = a + b
     return c
 
+def prod(a,b):
+    c = a*b
+    return c
+
 print()
 ans = sum(3,4)
 print('sum =', ans)
+
+print()
+ans = prod(3,4)
+print('product = ', ans)
 
 # end add.py
